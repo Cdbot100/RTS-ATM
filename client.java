9bdeafcc82a3c6798cbc4ab10d1216dc9f2a238a
Scott <Scott@MBP.local>
2017-07-25 10:31:17 -0500

    other files

---
+++ client.java
@@ -0,0 +1,12 @@
+public class Client {
+	public static void SendServiceRequest (String args[]) {
+		System.out.println ("Hello World!");
+    }
+    	public static void ReadResponse (String args[]) {
+		System.out.println ("Hello World!");
+    }
+    	public static void Connector1Send (String args[]) {
+		System.out.println ("Hello World!");
+	}
+
+}
