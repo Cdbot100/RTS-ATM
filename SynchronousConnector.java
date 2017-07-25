563d0e43395ee640f01def05222996f15fcedc63
Scott <Scott@MBP.local>
2017-07-25 11:49:10 -0500

    arraylists

--- SynchronousConnector.java
+++ SynchronousConnector.java
@@ -1,7 +1,10 @@
 import java.io.*;
+import java.util.*;
 class SynchronousConnector extends message{
 private boolean messageBufferFull = false;
 private boolean responseBufferFull = false;
+private ArrayList <message> messageBuffer = new ArrayList<message>();
+
 
     public void send (message in, message out){
         //place message in buffer;
