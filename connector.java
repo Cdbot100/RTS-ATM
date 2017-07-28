734f4afc239f3ee65c2743e0e14af3c2df1aae3e
CS-Samuel <samuelweems@gmail.com>
2017-07-28 16:16:33 -0500

    Add files via upload

---
+++ connector.java
@@ -0,0 +1,97 @@
+public class connector 
+{
+
+    private boolean requestBufferFull;
+    private boolean responseBufferFull; 
+    private OurMessage requestBuffer = new OurMessage();
+    private OurMessage responseBuffer = new OurMessage();
+    private client client = new client();
+    private server server = new server();
+
+// Constructor
+
+    public void connector()
+    {
+        requestBufferFull = false;
+        responseBufferFull= false;
+
+    }
+
+// Methods
+
+    public void setClient (client newClient){
+        client = newClient;
+
+    }
+
+    public void setServer (server newServer){
+        server = newServer;
+
+    }
+
+    public OurMessage send (OurMessage request)
+    {
+        requestBuffer = request;
+        requestBufferFull = true;
+    
+        synchronized (server)
+        {
+            server.notify();
+        }
+       synchronized (client)
+       { 
+            try
+            {
+                while (!responseBufferFull)
+                {
+                    client.wait();
+                }
+            }
+            catch(InterruptedException c)
+            {
+           
+            //throw new RuntimeException(c);
+            }
+       }
+
+        responseBufferFull = false;
+        return responseBuffer;
+    }
+
+    public OurMessage receive ()
+    {
+        synchronized (server)
+        {
+            try
+            {
+                while (!requestBufferFull)
+                {
+                    server.wait();
+                }
+            }
+            catch(InterruptedException e){}
+         
+        }
+
+        requestBufferFull = false;
+        return requestBuffer;
+    }
+
+    public void reply (OurMessage response)
+    {
+        
+        responseBuffer = response;
+        responseBufferFull = true;
+        synchronized (client)
+        {
+            client.notify();
+    
+        }
+    }
+
+}
+
+
+
+
+
