95f199935421061df08f6c0c55b584d0eb6b9184
CS-Samuel <samuelweems@gmail.com>
2017-07-31 13:02:46 -0500

    Add files via upload

---
+++ OurMessage.java
@@ -0,0 +1,18 @@
+// requestType: Types of Requests sent from Client to Server
+//      0 - Debit Card PIN Check
+//      1 - Check Balance
+//      2 - Deposit
+//      3 - Withdraw
+//      4 - Transfer
+//      *5 - Error Response
+//      *6 - Valid PIN Response 
+//      
+
+public class OurMessage {
+    public int requestType, Account, pin, transferAccount;
+    public float Balance;
+    public float transactionAmount;
+
+    
+
+}
